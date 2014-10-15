d06b9a1a54291b1953db6bfd181d91a6afdbf66a
itishi14 <itishi14@gmail.com>
2014-10-15 22:55:33 +0900

    kinectunity

---
+++ KnectWall/Assets/Wall Script/MoveWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0,0,0.09f);
+		//Destroy(gameObject, 8.0f);
+	}
+
+	private void OnCollisionEnter(Collision collision){
+				
+		if (collision.gameObject.tag == "Human") {
+			Destroy(gameObject);
+						Debug.Log ("hit");
+				}
+
+		if (collision.gameObject.tag == "Delete") {
+						Destroy (gameObject);
+				}
+		}
+}
